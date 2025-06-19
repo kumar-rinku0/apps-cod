d402bae595834f71f14243ad41e61d4948ab5e6a
rinku kumar <rinkukumar412616@gmail.com>
2025-06-19 09:16:21 +0530

    gitignore added and audit fixed added

--- attendance-management/front-end/src/providers/AuthProvider.jsx
+++ attendance-management/front-end/src/providers/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useEffect } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
 // Create Context
